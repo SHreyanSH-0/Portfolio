bbb699b6c898a73579fed7056feed1a32d729d9e
Shreyansh Tiwari <tiwarishreyansh013@gmail.com>
2026-07-18 11:37:00 +0530

    backend_fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import AI from "./pages/AI";
 
 function App() {
+    fetch(import.meta.env.VITE_BACKEND_URL);
     return (
         <Routes>
             <Route path="/" element={<Home />} />
